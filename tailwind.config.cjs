238ddc8a70a29a53eaadfdac49281ad81599023c
Lisa Grant <lisagrant1990@gmail.com>
2022-11-17 14:50:19 +0100

    add eslint, prettier and a svg logo,also started on navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        colors: {
+            mainColor: '#314657',
+            SecondColor: '#CCA152',
+            thirdColor: '#9DB3BE',
+            fourthColor: '#58626E',
+        },
+        extend: {},
+    },
 };
