f7d5678d1ed5a41583c46ac6a1476f9e18e73f58
Lisa Grant <lisagrant1990@gmail.com>
2022-12-07 12:07:24 +0100

    removed nav.js as it was no need and the content is added to main.js

--- js/main.js
+++ js/main.js
@@ -14,6 +14,19 @@ console.log(userName);
 console.log(accessToken);
 console.log(avatar);
 
+//header and Nav
+
+const mobileMenuBtn = document.querySelector('#Mobile-menu-btn');
+const mobielMenu = document.querySelector('.mobile-menu');
+const xclose = document.querySelector('#x');
+const menu = document.querySelector('#menu');
+
+mobileMenuBtn.addEventListener('click', () => {
+    mobielMenu.classList.toggle('hidden');
+    xclose.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
+});
+
 const logOutBtnMobil = document.querySelector('#logoutbtnMobil');
 const logOutBtn = document.querySelector('#logoutbtn');
 if (logOutBtn) {
